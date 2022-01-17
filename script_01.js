ae3fa06ed951745836ba87629de3fd981070c475
Amir <amir.tehrani@hotmail.de>
2022-01-17 09:19:32 +0100

    deklaration +  zuweisung

--- script_01.js
+++ script_01.js
@@ -3,5 +3,21 @@
 /***** Variablen 01 *******/
 
 
+/***
+ *  Block-Kommentar
+ * 
+ * 
+ */
 
+// Konsolenausgabe
 
+// console.log("Hello World!"); 
+
+let firstName;  // Deklaration (Definition)
+firstName = "Max";  // Wertzuweisung 
+console.log(firstName); // Ausgabe 
+
+let familyName = "Mütze"; // Deklartion + Wertzuweisung 
+console.log(familyName); 
+
+console.log(firstName + " " + familyName); 
